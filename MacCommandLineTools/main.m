32246b1d03e6f9a41a72fff589839b86b72e90bb
123456 <liweifeng@myoas.com>
2020-07-27 15:29:56 +0800

    优化、心跳

--- MacCommandLineTools/main.m
+++ MacCommandLineTools/main.m
@@ -66,7 +66,7 @@ void autoDaka(){
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         ///选择任务
-        printf("请输入任务类型?(回车任务执行):\n1:自动点点啥\n2:间隔时间打开某程序(如每15分钟打开TeamTalk查看消息)\n");
+        printf("请输入任务类型?(回车任务执行):\n1:点啥\n2:看啥\n");
         BOOL selectOK = false;//输入正确否
         int selectType = 0;//输入类型
         while (selectOK == false) {
@@ -85,22 +85,21 @@ int main(int argc, const char * argv[]) {
         switch (selectType) {
             case 1:
                 {
+                    ///维持心跳的鼠标点击事件
+                    CGEventRef theEvent = CGEventCreateMouseEvent(NULL, kCGEventLeftMouseDown, CGPointMake(1318, 471), kCGMouseButtonLeft);
                     ///关闭浏览器
                     runCommandLine(@"pkill firefox");
                     ///关闭图片
                     runCommandLine(@"pkill Preview");
-                    ///打开图片
+                    ///打开图片并全屏
                     runCommandLine(@"open /Users/w9005556/Desktop/black.png");
                     sleep(10);
-                    keyboardClick(3, kCGEventFlagMaskCommand ^ kCGEventFlagMaskControl);//全屏
-                    ///随机分秒
-                    int randomMin,randomSec;
-                    randomMin = arc4random()%20 + 40;
-                    randomSec = arc4random()%60;
+                    keyboardClick(3, kCGEventFlagMaskCommand ^ kCGEventFlagMaskControl);
                     sleep(3);
                     ///轮询判断
                     while (1) {
                         sleep(1);
+                        ///获取当前时分秒
                         struct tm *p;
                         time_t ptime;
                         time(&ptime);
@@ -109,30 +108,30 @@ int main(int argc, const char * argv[]) {
                         currentHour = p->tm_hour + 8;
                         currentMin = p->tm_min;
                         currentSec = p->tm_sec;
-                        if (((currentHour == 8)&&(currentMin == randomMin)&&(currentSec == randomSec))||((currentHour == 19)&&(currentMin == randomMin)&&(currentSec == randomSec))) {
-                            ///关闭图片
+                        ///打
+                        if (((currentHour == 15)&&(currentMin == 25))||((currentHour == 19)&&(currentMin == 15))) {
                             runCommandLine(@"pkill Preview");
                             sleep(3);
                             runCommandLine(@"open -a \"/Applications/firefox.app\" --args  --kiosk  'https://oms.myoas.com'");
-                            sleep(30);
+                            sleep(40);
                             autoDaka();
-                            //重新生成随机分秒
-                            randomMin = arc4random()%20 + 40;
-                            randomSec = arc4random()%60;
+                        }
+                        ///跳
+                        if (currentSec == 48) {
+                            mouseClick(theEvent);
                         }
                     }
                 }
                 break;
             case 2:
                 {
-                    printf("任务已开始,control+c终止任务");
-                    while (1) {
-                        runCommandLine(@"open /Applications/TeamTalk.app");
-                        sleep(3);
-                        keyboardClick(48, kCGEventFlagMaskCommand);//切回当前程序
-                        sleep(1800);
-                    }
-                    
+//                    printf("任务已开始,control+c终止任务");
+//                    while (1) {
+//                        runCommandLine(@"open /Applications/TeamTalk.app");
+//                        sleep(3);
+//                        keyboardClick(48, kCGEventFlagMaskCommand);//切回当前程序
+//                        sleep(1800);
+//                    }
                 }
                 break;
             default:
